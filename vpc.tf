6091850c85868eab8a558ae0eb153cb042a76f4a
diogofiaminghi <diogofiaminghi@gmail.com>
2023-02-03 11:54:26 -0300

    IGW Created

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,9 @@
 resource "aws_vpc" "this" {
-  cidr_block = "10.17.0.0/16"
-  tags       = merge(local.common_tags, { Name = "VPC-terraform-application-auto-scaling" })
+  cidr_block       = "10.17.0.0/16"
+  tags = merge(local.common_tags, { Name = "VPC-terraform-application-auto-scaling" })
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags = merge(local.common_tags, { Name = "IGW-terraform-application-auto-scaling" })
 }
