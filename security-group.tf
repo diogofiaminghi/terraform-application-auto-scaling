5eaf1d538c83f3c8f5ad081b260d20a63880989c
diogofiaminghi <diogofiaminghi@gmail.com>
2023-02-03 16:36:45 -0300

    Security Groups Created

---
+++ security-group.tf
@@ -0,0 +1,78 @@
+resource "aws_security_group" "web" {
+  name        = "SG-WEB-terraform-application-auto-scaling"
+  description = "Allow public inbound traffic"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 80 # http
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443 # https
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.this["pvt_a"].cidr_block]
+  }
+
+  tags = merge(local.common_tags, { Name = "SG-WEB-terraform-application-auto-scaling" })
+}
+
+resource "aws_security_group" "db" {
+  name        = "SG-DB-terraform-application-auto-scaling"
+  description = "Allow incoming database connections"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.this.cidr_block]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.this.cidr_block]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.common_tags, { Name = "SG-DB-terraform-application-auto-scaling" })
+}
